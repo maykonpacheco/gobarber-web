78182f43357db747d46ab20da46f67dcaf95fe7a
maykonpacheco <maykonpachecogeo@gmail.com>
2020-03-19 16:03:21 -0400

    Validações

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,10 +1,18 @@
 import React from "react";
-
 import { Link } from "react-router-dom";
-
 import { Form, Input } from "@rocketseat/unform";
-
+import * as Yup from "yup";
 import logo from "~/assets/logo.svg";
+
+const schema = Yup.object().shape({
+  name: Yup.string().required("O nome é obrigatório"),
+  email: Yup.string()
+    .email("Insira um e-mail válido")
+    .required("O e-mail é obrigatório"),
+  password: Yup.string()
+    .min(6, "No mínimo 6 caracteres")
+    .required("A senha é obrigatório")
+});
 
 export default function SignUp() {
   function handleSubmit(data) {
@@ -14,7 +22,7 @@ export default function SignUp() {
     <>
       <img src={logo} alt="Gobarber" />
 
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input
