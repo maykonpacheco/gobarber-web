a699fa6012e7fa9ff93dc43cd51ecafead66e39f
maykonpacheco <maykonpachecogeo@gmail.com>
2020-03-20 16:32:24 -0400

    Notificações

--- src/components/Notifications/styles.js
+++ src/components/Notifications/styles.js
@@ -34,6 +34,7 @@ export const NotificationList = styled.div`
   background: rgba(0, 0, 0, 0.6);
   border-radius: 4px;
   padding: 20px 5px;
+  display: ${props => (props.visible ? "block" : "none")};
 
   &::before {
     content: "";
@@ -64,8 +65,10 @@ export const Notification = styled.div`
   }
 
   time {
+    display: block;
     font-size: 12px;
     opacity: 0.6;
+    margin-bottom: 5px;
   }
 
   button {
@@ -73,9 +76,6 @@ export const Notification = styled.div`
     border: 0;
     background: none;
     color: ${lighten(0.2, "#7159c1")};
-    padding: 0 5px;
-    margin: 0 5px;
-    border-left: 1px solid rgba(255, 255, 255, 0.1);
   }
 
   ${props =>
@@ -88,6 +88,7 @@ export const Notification = styled.div`
         height: 8px;
         background: #ff892e;
         border-radius: 50%;
+        margin-left: 10px;
       }
     `}
 `;
